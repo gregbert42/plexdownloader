16cc3d0839753e12a33f866001b041d3cfb4a018
gregbert42 <gregbert@Fractal.hersches.com>
2020-04-19 11:17:39 +0200

    april 19

--- plexdownloader.py
+++ plexdownloader.py
@@ -3,7 +3,7 @@ from plexapi import utils
 import sys,json,pprint,os
 import downloader
 
-#Future improvement: show size before downloading...
+#create directory with movie title
 
 def crypt(direction,input,key):
     from cryptography.fernet import Fernet
@@ -37,13 +37,13 @@ def search(server,target):
     results = server.search(target)
     items = [i for i in results if i.__class__ in VALID_TYPES] #imported code
     if (len(items)==0): return False  #in case nothing is found, abort the loop, dont ask to make a choice
-    items = utils.choose('Choose result', items, lambda x: '(%s) %s %s' % (x.type.title(), x.title[0:60], x.title[0:60])) #imported code
+    items = utils.choose('Choose your item: ', items, lambda x: '(%s) %s' % (x.type.title(), x.title[0:60])) #imported code
     if not isinstance(items, list): items = [items]  #converts items to a list if it already isnt a list
     item=[]
     for i in items:
         if isinstance(i, Show):
             display = lambda i: '%s %s %s' % (i.grandparentTitle, i.seasonEpisode, i.title)
-            selected_eps = utils.choose('Choose episode', i.episodes(), display)
+            selected_eps = utils.choose('Choose episode: ', i.episodes(), display)
             if isinstance(selected_eps, list): item += selected_eps
             else: item.append(selected_eps)
         else: item.append(i) #If its not a show (doesnt have episodes), just adds directly
@@ -70,24 +70,47 @@ def connect_plex(credentials):
     print ("Connected")
     return (server)
 
+def inspct(thing):
+    try:
+        print (thing)
+        print ("Dict-->",thing.__dict__)
+        print ("Keys-->",thing.__dict__.keys())
+    except:
+        print("Error:", sys.exc_info())
+        return False
+    return True
+
 def add_items(items):
     items_to_dl=[]
     item_row={}
 #    print ("I've been given this to download",items)
     for item in items:
         for part in item.iterParts():
-            filename = '%s.%s' % (item._prettyfilename(), part.container) # borrowed code, next few lines
-            url = item._server.url('%s?download=1' % part.key)
-            print ("Filename: %s\t%s" % (filename,item.title))
-            query = input ("Is this the file you want? (y/n/exit): ")
-            if (query[:1].lower() == "y"):
-                item_row['url']=url
-                item_row['token']=item._server._token
-                item_row['filename']=filename
-                item_row['session']=item._server._session
-                items_to_dl.append(item_row)
-            elif (query.lower() == 'exit'): filepaths='exit'
-    return (items_to_dl) 
+            go = True
+            while (go):
+                filename = '%s.%s' % (item._prettyfilename(), part.container) # borrowed code, next few lines
+                url = item._server.url('%s?download=1' % part.key)
+                print ("Filename: %s\t%s\t%.2f MB" % (filename,item.title,part.size/1E6))
+                query = input ("Add this file to queue? (y/n/exit): ")
+                if (query[:1].lower() == "y"):
+                    splitfile = part.file.split('/')
+                    dirname = splitfile[len(splitfile)-2]
+                    item_row['dir']=dirname
+                    item_row['url']=url
+                    item_row['token']=item._server._token
+                    item_row['filename']=filename
+                    item_row['session']=item._server._session
+                    item_row['type']=item.type
+                    items_to_dl.append(item_row)
+                    go = False
+                elif (query.lower() == 'exit'): return 'exit'
+                elif (query.lower() == 'inspect'):
+                    print ("Inspection of item:")
+                    inspct(item)
+                    print ("Inspection of part:")
+                    inspct(part)
+                elif (query[:1].lower() == 'n'): go = False
+    return (items_to_dl)
 
 def download_items(items_to_dl):
     rate_limit = input("Enter download speed limit, or press zero for none (in kb/sec): ")
@@ -98,8 +121,9 @@ def download_items(items_to_dl):
         token=row['token']
         filename=row['filename']
         session=row['session']
-        downloader.download_with_rate(url, token=token, filename=filename, savepath=os.getcwd(),  #token by my trial and error
-                                       session=session, showstatus=True, rate_limit=rate_limit)
+        savepath=os.getcwd()
+        if (row['type'] == "movie"): savepath=savepath + '/' + row['dir']
+        downloader.download_with_rate(url, token=token, filename=filename, savepath=savepath,  session=session, showstatus=True, rate_limit=rate_limit)
     return
 
 def main(args):
